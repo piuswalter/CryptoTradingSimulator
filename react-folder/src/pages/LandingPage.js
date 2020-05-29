07b82d665b78f83c3096099191272eaa8315a26b
Pius Walter <pius.walter@gmx.net>
2020-05-29 20:40:15 +0200

    Changed that dashboard can only be displayed after login and outomatix forwarding and logout

--- react-folder/src/pages/LandingPage.js
+++ react-folder/src/pages/LandingPage.js
@@ -1,31 +1,48 @@
 import React from 'react';
+import AuthService from '../services/auth.service'
 import { Container, Row, Col, Image, Button, Navbar } from 'react-bootstrap';
 import { Wave, Portfolio, Logo } from '../img'
 
 //Component LandingPage
-export default function LandingPage() {
-    return (
-        <Container fluid>
+export default class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: AuthService.getCurrentUser()
+        };
+    }
 
-            <Image src={Wave} className='position-fixed h-100'></Image>
+    render() {
+        const { currentUser } = this.state;
 
-            <Navbar className='z-100'>
-                <Navbar.Brand href='./'><img src={Logo} alt='PaperCoin' /></Navbar.Brand>
-            </Navbar>
+        if (currentUser != null) {
+            this.props.history.push("/dashboard");
+            window.location.reload();
+        }
 
-            <Row className='main-content'>
-                <Col className='d-flex' style={{ pointerEvents: 'none' }}>
-                    <Image src={Portfolio} className='mx-auto my-auto h-60' fluid></Image>
-                </Col>
-                <Col className='d-flex'>
-                    <div className='mx-auto my-auto'>
-                        <h1>Build Your Portfolio</h1>
-                        <h3>Papertrading with cryptocurrencies</h3>
-                        <Button variant='primary' size='lg' className='mt-3' href='./login'>Get Started!</Button>
-                    </div>
-                </Col>
-            </Row>
-
-        </Container >
-    )
+        return (
+            <Container fluid>
+    
+                <Image src={Wave} className='position-fixed h-100'></Image>
+    
+                <Navbar className='z-100'>
+                    <Navbar.Brand href='./'><img src={Logo} alt='PaperCoin' /></Navbar.Brand>
+                </Navbar>
+    
+                <Row className='main-content'>
+                    <Col className='d-flex' style={{ pointerEvents: 'none' }}>
+                        <Image src={Portfolio} className='mx-auto my-auto h-60' fluid></Image>
+                    </Col>
+                    <Col className='d-flex'>
+                        <div className='mx-auto my-auto'>
+                            <h1>Build Your Portfolio</h1>
+                            <h3>Papertrading with cryptocurrencies</h3>
+                            <Button variant='primary' size='lg' className='mt-3' href='./login'>Get Started!</Button>
+                        </div>
+                    </Col>
+                </Row>
+    
+            </Container >
+        )
+    }
 }
