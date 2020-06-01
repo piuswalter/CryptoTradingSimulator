68f428b6aaadb7e47029926bdc166295f5c0052f
Golo Muehr <golomuehr@MacBook-Pro-2.local>
2020-06-01 12:46:46 +0200

    added user.service to query backend for buy sell and getbalances

--- react-folder/src/services/auth.service.js
+++ react-folder/src/services/auth.service.js
@@ -31,7 +31,7 @@ class AuthService {
     }
 
     getCurrentUser() {
-        return JSON.parse(localStorage.getItem('user'));;
+        return JSON.parse(localStorage.getItem('user'));
     }
 }
 
