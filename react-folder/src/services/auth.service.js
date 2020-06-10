e7e4bb1bb0534dfb7e0cea109ddb1667657b2818
Pius Walter <pius.walter@gmx.net>
2020-06-10 07:20:52 +0200

    Changed static ip address to localhost and removed mongodata folder for empty database

--- react-folder/src/services/auth.service.js
+++ react-folder/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.1.112:8080/auth/";
+const API_URL = "http://localhost:8080/auth/";
 
 class AuthService {
     login(username, password) {
--- react-folder/src/services/exchange.service.js
+++ react-folder/src/services/exchange.service.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const API_URL = "http://192.168.1.112:8080/exchange";
+const API_URL = "http://localhost:8080/exchange";
 
 class ExchangeService {
     getCurrentPrice(coin) {
