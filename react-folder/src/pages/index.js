47206eb7b541740a23f1dddffd31c24fc44d0968
Pius Walter <pius.walter@gmx.net>
2021-05-17 16:12:14 +0200

    Renamed frontend folder

--- react-folder/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import LandingPage from './LandingPage';
-import Login from './Login';
-import Register from './Register';
-import Dashboard from './Dashboard';
-import Coin from './Coin';
-import About from './About';
-
-export { LandingPage, Login, Register, Dashboard, Coin, About };
--- react-folder/src/services/index.js
+++
@@ -1,5 +0,0 @@
-import AuthService from './auth.service';
-import ExchangeService from './exchange.service';
-import UserService from './user.service';
-
-export { AuthService, ExchangeService, UserService };
